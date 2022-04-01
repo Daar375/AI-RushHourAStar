5f076a50ec1b5e50d89da7c01663ec078a4ba3ec
lombax <anner.calvo@gmail.com>
2022-03-31 20:07:12 -0600

    Se añadió el tipo al vehículo

--- AI-RushHourAStar/src/rushhour/Vehicle.java
+++ AI-RushHourAStar/src/rushhour/Vehicle.java
@@ -1,11 +1,13 @@
 package rushhour;
 public class Vehicle {
     private String orientation;
+    public String type;
     private int size;
     private int posX;
     private int posY;
 
-    public Vehicle(String orientation, int size, int posX, int posY) {
+    public Vehicle(String type, String orientation, int size, int posX, int posY) {
+        this.type = type;
         this.orientation = orientation;
         this.size = size;
         this.posX = posX;
