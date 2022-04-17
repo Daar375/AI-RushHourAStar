8ebe9ff64ac6bffd1fd06162de5e90e00bad2e50
Cryosis-stat <josepabloms@gmail.com>
2022-04-16 23:17:35 -0600

    Revert "Merge branch 'RamaAnnerv2' into main"
    This reverts commit 7f0864ffb0b1b5a25f9e0fcefd3add863a1dc75d, reversing
    changes made to e1ef9fe10f92f1e093cf7eb25de532b0d1be9b53.

--- AI-RushHourAStar/src/rushhour/Astar.java
+++ AI-RushHourAStar/src/rushhour/Astar.java
@@ -25,7 +25,7 @@ public class Astar {
         Puzzle nodo;
         opened.add(puzzle);
         
-        while(!opened.isEmpty()) {
+        while(loop) {
 
             current = opened.get(0);
 
